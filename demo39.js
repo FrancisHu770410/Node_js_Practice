96403a0c4deb43826c5ae53d45dfa1f16e73276e
FrancisHu <FrancisHu@systex.com>
2021-05-05 16:45:17 +0800

    use arrow function

--- demo39.js
+++ demo39.js
@@ -4,18 +4,17 @@ function FileObject() {
     this.fileName = "";
     this.file_exists = function (callback) {
         console.log("about to open:", this.fileName)
-        const holdThis = this;
-        fs.open(this.fileName, 'r', function (err, handle) {
+        fs.open(this.fileName, 'r',  (err, handle) => {
             if (err) {
-                console.log("can not open file", holdThis.fileName)
-                callback(err)
+                console.log("can not open file", this.fileName)
+                callback(err)        
                 return;
             }
-            console.log("the file we process is:", holdThis.fileName)
+            console.log("the file we process is:", this.fileName)
             console.log("now we can use file handler:", handle)
             fs.close(handle, function () { })
             callback(null, true);
-
+            
         })
         console.log("this will no longer exist")
     }
