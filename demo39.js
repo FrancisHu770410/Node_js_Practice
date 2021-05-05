eb04dd31cb44bea329d0413974015f73fc5d273f
FrancisHu <FrancisHu@systex.com>
2021-05-05 16:44:55 +0800

    use hold this

--- demo39.js
+++ demo39.js
@@ -1,20 +1,21 @@
 const fs = require('fs')
-const path1 = "datax\\info.txt"
+const path1 = "data\\info.txt"
 function FileObject() {
     this.fileName = "";
     this.file_exists = function (callback) {
         console.log("about to open:", this.fileName)
+        const holdThis = this;
         fs.open(this.fileName, 'r', function (err, handle) {
             if (err) {
-                console.log("can not open file", this.fileName)
-                callback(err)        
+                console.log("can not open file", holdThis.fileName)
+                callback(err)
                 return;
             }
-            console.log("the file we process is:", this.fileName)
+            console.log("the file we process is:", holdThis.fileName)
             console.log("now we can use file handler:", handle)
             fs.close(handle, function () { })
             callback(null, true);
-            
+
         })
         console.log("this will no longer exist")
     }
