72805936d19a8c3000b2a34b46e7e0bd6738ecbd
FrancisHu <FrancisHu@systex.com>
2021-05-05 17:31:48 +0800

    finished

---
+++ demo42.js
@@ -0,0 +1,16 @@
+buf = new Buffer.alloc(20);
+len = buf.write("ABC")
+buf2 = Buffer.from([97, 98, 99])
+buf3 = Buffer.from("Hello World")
+buf4 = Buffer.from("測試123")
+
+console.log("dump buffer directly:", buf.toString('hex', 0, 3))
+console.log("dump buffer directly:", buf.toString('hex'))
+console.log("dump buffer directly:", buf.toString('utf-8'))
+console.log("buffer2:", buf2.toString('hex'))
+console.log("buffer2:", buf2.toString('ascii'))
+console.log("buffer3:", buf3.toString('ascii'))
+console.log("buffer3:", buf3.toString('utf-8'))
+console.log("buffer4:", buf4.toString('hex'))
+console.log("buffer4:", buf4.toString('ascii'))
+console.log("buffer4:", buf4.toString('utf-8'))
